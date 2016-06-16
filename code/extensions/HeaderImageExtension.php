d049648590056eede79e486c1564f51e97922b90
Jason Irish <jirish@dynamicagency.com>
2016-06-15 21:23:42 -0500

    PreviewExtension, HeaderImageExtension

---
+++ code/extensions/HeaderImageExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+class HeaderImageExtension extends DataExtension
+{
+    private static $has_one = array(
+        'HeaderImage' => 'Image',
+    );
+    
+    public function updateCMSFields(FieldList $fields)
+    {
+        $ImageField = UploadField::create('HeaderImage', 'Header Image')
+            ->setFolderName('Uploads/HeaderImages')
+            ->setConfig('allowedMaxFileNumber', 1)
+        ;
+        $ImageField->getValidator()->allowedExtensions = array('jpg', 'jpeg', 'gif', 'png');
+        $ImageField->getValidator()->setAllowedMaxFileSize(CORE_TOOLS_IMAGE_SIZE_LIMIT);
+        $fields->addFieldsToTab('Root.Images', array(
+            $ImageField,
+        ));
+    }
+}
