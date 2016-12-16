85716a4dedaa165a170879e1fc5299aa1d004bc7
Nic Horstmeier <nhorstmeier@gmail.com>
2016-12-16 15:03:35 -0600

    Test update

--- tests/extensions/CollectionExtensionTest.php
+++ tests/extensions/CollectionExtensionTest.php
@@ -17,9 +17,9 @@ class CollectionExtensionTest extends CoreToolsTest
      */
     public function testGetCollection()
     {
-        $object = $this->objFromFixture('Dynamic\\CoreTools\\Tests\\TestPage', 'default');
+        $object = $this->objFromFixture('Dynamic\CoreTools\Tests\TestPage', 'default');
         $controller = TestPage_Controller::create($object);
-        $this->assertInstanceOf('SilverStripe\\ORM\\DataList', $controller->getCollection());
+        $this->assertInstanceOf('SilverStripe\ORM\DataList', $controller->getCollection());
 
         $object = $controller->config()->managed_object;
         $this->assertInstanceOf($object, $controller->getCollection()->first());
