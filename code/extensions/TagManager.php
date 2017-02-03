b3fd0300148398cb64a83ef88802da0bfc3db22f
Jason Irish <jirish@dynamicagency.com>
2017-02-03 10:41:44 -0600

    Tag - rename to CoreTag

--- code/extensions/TagManager.php
+++ code/extensions/TagManager.php
@@ -5,7 +5,7 @@ class TagManager extends DataExtension
     public function updateCMSFields(FieldList $fields)
     {
         if ($this->owner->exists()) {
-            $tagField = TagField::create('Tags', 'Tags', Tag::get(), $this->owner->Tags())
+            $tagField = TagField::create('Tags', 'Tags', CoreTag::get(), $this->owner->Tags())
                 ->setShouldLazyLoad(true)
                 ->setCanCreate(true)
             ;
