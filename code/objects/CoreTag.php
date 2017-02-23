d5e81c392c64e37fc1955fe24a6e905066054d5c
Jason Irish <jirish@dynamicagency.com>
2017-02-22 19:09:19 -0600

    Revert "Revert "Tag - rename to CoreTag""
    This reverts commit 73bc53236136f7c0b298baedd845ce63b02ab92c.

--- code/objects/CoreTag.php
+++ code/objects/CoreTag.php
@@ -1,6 +1,6 @@
 <?php
 
-class Tag extends DataObject
+class CoreTag extends DataObject
 {
     /**
      * @var array
@@ -24,7 +24,7 @@ class Tag extends DataObject
         $result = parent::validate();
 
         if (!$this->Title) {
-            $result->error('Title is requied before you can save');
+            $result->error('Title is required before you can save');
         }
 
         return $result;
--- tests/CoreToolsTest.php
+++ tests/CoreToolsTest.php
@@ -69,7 +69,7 @@ class TestPage extends Page implements TestOnly
     private static $many_many = array(
         'Promos' => 'Promo',
         'Videos' => 'YouTubeVideo',
-        'Tags' => 'Tag',
+        'Tags' => 'CoreTag',
     );
 
     private static $many_many_extraFields = array(
