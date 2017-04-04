150036aa78e9e21e07f3829abd8ddaee8a7d6af6
Nic Horstmeier <nhorstmeier@gmail.com>
2017-04-03 20:44:55 -0500

    UPDATE bootstrapping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,8 +6,17 @@
  * @author  Dynamic <dev@dynamicagency.com>
  */
 
+define('CORE_TOOLS_BASE_DIR', realpath(__DIR__ . '/..'));
 require '../vendor/autoload.php';
 
 global $_FILE_TO_URL_MAPPING;
+$_FILE_TO_URL_MAPPING[CORE_TOOLS_BASE_DIR] = 'http://localhost';
 
 global $databaseConfig;
+$databaseConfig = [
+  'type'     => 'MySQLDatabase',
+  'server'   => '127.0.0.1',
+  'username' => 'root',
+  'password' => '',
+  'database' => 'core-tools-tests'
+];
