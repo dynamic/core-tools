84edb6237cc58bf783812738d66a7e1038eb0d1c
Nic Horstmeier <nhorstmeier@dynamicagency.com>
2018-04-27 16:20:17 -0500

    ENHANCEMENT CTEmpbedShortcodeProvider
    This adds additional bootstrap classes to the embed’s container <div>

--- _config.php
+++ _config.php
@@ -5,3 +5,16 @@ define('CORE_TOOLS_DIR', basename(__DIR__));
 
 // set image upload max size
 define('CORE_TOOLS_IMAGE_SIZE_LIMIT', '1024000');
+
+\SilverStripe\View\Parsers\ShortcodeParser::get('default')->unregister('embed');
+\SilverStripe\View\Parsers\ShortcodeParser::get('default')->register(
+    'embed',
+    function ($arguments, $content = null, $parser = null, $tagName) {
+        return \Dynamic\CoreTools\View\CTEmbedShortcodeProvider::handle_shortcode(
+            $arguments,
+            $content,
+            $parser,
+            $tagName
+        );
+    }
+);
