e21e39b9b8e0c462c654e33fb500054df751151a
Jason Irish <jirish@dynamicagency.com>
2018-06-15 14:21:10 -0500

    feature - data extension to add Linkable to SlideImage

---
+++ src/ORM/SlideImageDataExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dynamic\CoreTools\ORM;
+
+use Sheadawson\Linkable\Forms\LinkField;
+use Sheadawson\Linkable\Models\Link;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataExtension;
+
+class SlideImageDataExtension extends DataExtension
+{
+    /**
+     * @var array
+     */
+    private static $has_one = [
+          'SlideLink' => Link::class,
+    ];
+
+    /**
+     * @param FieldList $fields
+     */
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->removeByName([
+            'PageID',
+            'PageLinkID',
+            'SlideLinkID',
+        ]);
+
+        $fields->addFieldsToTab('Root.Main', [
+            LinkField::create('SlideLinkID', 'Link'),
+        ]);
+    }
+}
