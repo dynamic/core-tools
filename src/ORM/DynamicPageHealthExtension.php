94da4be6b3d4bcb2dc0dcf50fe58780314be39fe
Jason Irish <jsirish@users.noreply.github.com>
2018-09-14 09:57:29 -0500

    CMS Design, Elemental add-ons (#154)

---
+++ src/ORM/DynamicPageHealthExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Dynamic\CoreTools\ORM;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\ToggleCompositeField;
+use SilverStripe\ORM\DataExtension;
+use Vulcan\Seo\Extensions\PageHealthExtension;
+use Vulcan\Seo\Forms\GoogleSearchPreview;
+use Vulcan\Seo\Forms\HealthAnalysisField;
+
+class DynamicPageHealthExtension extends DataExtension
+{
+    public function updateCMSFields(FieldList $fields)
+    {
+        parent::updateCMSFields($fields);
+
+        if ($this->owner instanceof \SilverStripe\ErrorPage\ErrorPage) {
+            return;
+        }
+
+        $fields->addFieldsToTab('Root.SEO', [
+            ToggleCompositeField::create(null, 'SEO Health Analysis', [
+                GoogleSearchPreview::create(
+                    'GoogleSearchPreview',
+                    'Search Preview',
+                    $this->getOwner(),
+                    $this->owner->getRenderedHtmlDomParser()
+                ),
+                TextField::create('FocusKeyword', 'Set focus keyword'),
+                HealthAnalysisField::create('ContentAnalysis', 'Content Analysis', $this->getOwner()),
+            ])
+        ]);
+    }
+}
