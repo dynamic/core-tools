d37ee8bf0274a6872ec5b34fc5c37be394fcf41a
Jason Irish <jsirish@users.noreply.github.com>
2018-12-24 15:04:57 -0600

    GlobalConfigMigration - add fields for CompanyConfig (#169)

--- src/Migration/GlobalConfigMigration.php
+++ src/Migration/GlobalConfigMigration.php
@@ -3,26 +3,18 @@
 namespace Dynamic\CoreTools\Migration;
 
 use Dynamic\CompanyConfig\Model\CompanyConfigSetting;
-use Dynamic\TemplateConfig\Model\NavigationColumn;
 use Dynamic\TemplateConfig\Model\TemplateConfigSetting;
 use SilverStripe\CMS\Model\SiteTree;
-use SilverStripe\Dev\Debug;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
-use SilverStripe\Forms\GridField\GridFieldAddNewButton;
-use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
-use SilverStripe\Forms\GridField\GridFieldEditButton;
-use SilverStripe\Forms\HeaderField;
-use SilverStripe\Forms\LiteralField;
-use SilverStripe\ORM\DataExtension;
-use Symbiote\Addressable\Addressable;
-use Symbiote\Addressable\Geocodable;
-use Symbiote\GridFieldExtensions\GridFieldAddExistingSearchButton;
-use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 class GlobalConfigMigration extends DataExtension
 {
+    /**
+     * @var array
+     */
+    private static $db = array(
+        'CompanyName' => 'Varchar(200)',
+    );
+    
     /**
      * @var array
      */
@@ -45,9 +37,12 @@ class GlobalConfigMigration extends DataExtension
     public function onAfterWrite()
     {
         parent::onAfterWrite();
+
+        $company = CompanyConfigSetting::current_company_config();
+        $company->CompanyName = $this->owner->CompanyName;
+        $company->write();
         
         $template = TemplateConfigSetting::current_template_config();
-
         foreach ($this->owner->UtilityLinks() as $link) {
             $template->UtilityLinks()->add($link);
         }
