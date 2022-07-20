c0668274eaa93cbfe0979eb947fdbb39b71d1bdb
Nic <nhorstmeier@dynamicagency.com>
2022-07-20 10:45:23 -0500

    BUGFIX required param after optional param causes deprecation error

--- _config.php
+++ _config.php
@@ -9,7 +9,7 @@ define('CORE_TOOLS_IMAGE_SIZE_LIMIT', '1024000');
 \SilverStripe\View\Parsers\ShortcodeParser::get('default')->unregister('embed');
 \SilverStripe\View\Parsers\ShortcodeParser::get('default')->register(
     'embed',
-    function ($arguments, $content = null, $parser = null, $tagName) {
+    function ($arguments, $content = null, $parser = null, $tagName = null) {
         return \Dynamic\CoreTools\View\CTEmbedShortcodeProvider::handle_shortcode(
             $arguments,
             $content,
